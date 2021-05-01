b28a8281684c4f48b76284fad274c2b3a567537d
Jake <jacob.b.singer@gmail.com>
2021-05-01 11:27:17 -0400

    initial setup

---
+++ src/pages/landingpage.js
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+import Hero from "../components/hero";
+
+function LandingPage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default LandingPage;
